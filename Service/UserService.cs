3adc37dae605760268bbb03986b420ab1205366b
ELMURODSALOMOV <salomovelmurod062@gmail.com>
2024-03-31 23:43:08 +0500

    SignUp Validation

--- Service/UserService.cs
+++ Service/UserService.cs
@@ -3,6 +3,7 @@ using Register.Broker.Storeage;
 using Register.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,7 +28,38 @@ namespace Register.Service
 
         public Users SignUp(Users user)
         {
-           return this.storeageBroker.SignUpUser(user);
+           return user is null
+                ? SignUpUsersInvalid()
+                : ValidationAndSignUpUser(user);
+        }
+
+        private Users ValidationAndSignUpUser(Users user)
+        {
+            if(String.IsNullOrWhiteSpace(user.Name)
+              || String.IsNullOrWhiteSpace(user.Password))
+            {
+                this.loggingBroker.LogError("Invalid user information.");
+                return new Users();
+            }
+            else
+            {
+                var userInformation = this.storeageBroker.SignUpUser(user);
+                if(userInformation is null)
+                {
+                    this.loggingBroker.LogError("Not added user info");
+                }
+                else
+                {
+                    this.loggingBroker.LogInformation("Added user");
+                }
+                return userInformation;
+            }
+        }
+
+        private Users SignUpUsersInvalid()
+        {
+            this.loggingBroker.LogError("User information is null.");
+            return new Users();
         }
     }
 }
